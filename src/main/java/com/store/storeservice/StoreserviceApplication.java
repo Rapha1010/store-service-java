854a23891d11264fd11c6358af71d9c1ba32d970
Raphael Alves Tavares Leão <rapha.tavares1010@gmail.com>
2021-05-04 16:30:18 -0300

    Project Created

---
+++ src/main/java/com/store/storeservice/StoreserviceApplication.java
@@ -0,0 +1,13 @@
+package com.store.storeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/storeservice/StoreserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.storeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
