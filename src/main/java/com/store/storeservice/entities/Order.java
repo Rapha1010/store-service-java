008f164e840781ef7a004960bf16a74151b87e2d
Raphael Alves Tavares Leão <rapha.tavares1010@gmail.com>
2021-05-06 12:27:22 -0300

    Order Status Enum

--- src/main/java/com/store/storeservice/entities/Order.java
+++ src/main/java/com/store/storeservice/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.store.storeservice.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -28,14 +29,17 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	private Integer orderStatus;
 
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,  OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -53,6 +57,14 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) this.orderStatus = orderStatus.getCode();
 	}
 
 	public User getClient() {
