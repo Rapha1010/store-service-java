3d30810a29c3d85159a0b35364634283aeca6142
Raphael Alves Tavares Leão <rapha.tavares1010@gmail.com>
2021-05-10 11:31:47 -0300

    exceptions

--- src/main/java/com/store/storeservice/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/store/storeservice/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.store.storeservice.services.exceptions.DatabaseException;
 import com.store.storeservice.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,14 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
--- src/main/java/com/store/storeservice/services/UserService.java
+++ src/main/java/com/store/storeservice/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.store.storeservice.entities.User;
 import com.store.storeservice.repositories.UserRepository;
+import com.store.storeservice.services.exceptions.DatabaseException;
 import com.store.storeservice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,7 +36,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e ) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
